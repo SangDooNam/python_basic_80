07c8590304704c57aba6da85a40f819ad6c25210
SangDoo Nam <kexe009@hotmail.com>
2023-11-18 15:58:19 +0100

    finish

--- src/app.py
+++ src/app.py
@@ -5,9 +5,18 @@ def rnd(start, end):
     return randrange(start, end+1)
 
 # function should return the greatest number in a list
-def max_num_in_list( list ):
-    max = list[ 0 ]
-    for a in list:
-        if a < max:
-            max = a
-    return max
+def max_num_in_list( lst ):
+    for i in range(1, len(lst)):
+        key = lst[i]
+        j = i - 1
+        while j >= 0 and key < lst[j]:
+            lst[j + 1] = lst[j]
+            j -= 1
+        lst[j + 1] = key
+    
+    return lst[-1]
+        
+
+
+
+
--- src/test.py
+++ src/test.py
@@ -3,6 +3,17 @@ from app import rnd, max_num_in_list
 
 class TestApp(unittest.TestCase):
     # check if max_num_in_list will return right value
+    def test_rnd(self):
+        self.assertIn(rnd(start=2, end=20), [i for i in range(2, 21)])
+    '''
+    The randomized result is not deterministic, making it impossible to predict the exact output. 
+    However, by defining a specific range, you can ensure that the output 
+    will always fall within a predetermined set of values.
+    '''
+    def test_rnd_check_out_of_range(self):
+        self.assertNotIn(rnd(start=2, end=20), [0, 1]+[i for i in range(21, 50)])
+    
+    
     def test_max_num_in_list1(self):
         self.assertEqual(max_num_in_list([2, 6, 8, 7, -1]), 8, 'return value not the greatest value in max_num_in_list')
 
@@ -10,3 +21,5 @@ class TestApp(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+    
+    
